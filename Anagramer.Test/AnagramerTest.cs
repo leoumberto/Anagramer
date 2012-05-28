104cb77dee870bd8a57e3787bce8608797d01bdb
Leo Umberto <leoumberto@hotmail.com>
2012-05-28 16:06:28 -0300

    Primeiro commit

---
+++ Anagramer.Test/AnagramerTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Anagramer.Core;
+using NUnit.Framework;
+
+
+namespace Anagramer.Test
+{
+    [TestFixture]
+    public class AnagramerTest
+    {
+
+        [Test]
+        public void Posso_Ter_Anagrama()
+        {
+            var anagrama = new Anagrama();
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Deve_Existir_Uma_Palavra_Origem()
+        {
+            var anagrama = new Anagrama();
+
+            anagrama.palavraOrigem = "BIRO";
+
+            Assert.AreEqual("BIRO", anagrama.palavraOrigem);
+        }
+
+        [Test]
+        public void Palavra_Origem_Nao_Pode_Ser_Vazia()
+        {
+            var anagrama = new Anagrama();
+
+            anagrama.palavraOrigem = "";
+
+            Assert.Throws<Exception>(anagrama.VerificarSeNomeEhVazioOuNulo, "Palavra origem não pode ser vazia.");
+        }
+
+        [Test]
+        public void Palavra_Origem_Nao_Pode_Ser_Nula()
+        {
+            var anagrama = new Anagrama();
+
+            anagrama.palavraOrigem = null;
+
+            Assert.Throws<Exception>(anagrama.VerificarSeNomeEhVazioOuNulo, "Palavra origem não pode ser nula.");
+        }
+
+        [Test]
+        public void Palavra_Origem_Deve_Combinar_Letras_Para_Formar_Outras_Palavras()
+        {
+            var anagrama = new Anagrama();
+
+            anagrama.palavraOrigem = "BIRO";
+            anagrama.GerarAnagramas(anagrama.palavraOrigem);
+
+            anagrama.listaAnagramas.ForEach(c => Console.WriteLine(c));
+
+            Assert.Contains("BRIO", anagrama.listaAnagramas);
+        }
+
+        [Test]
+        public void Devem_ser_Formados_Numero_de_Palavras_Conforme_Combinacao_Matematica()
+        {
+            var anagrama = new Anagrama();
+
+            anagrama.palavraOrigem = "BIRO";
+            anagrama.GerarAnagramas(anagrama.palavraOrigem);
+
+            Assert.AreEqual(24, anagrama.ObterNumeroDeCombinacoes(anagrama.palavraOrigem));
+        }
+    }
+}
---
+++ Anagramer/Anagrama.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Anagramer.Core
+{
+    public class Anagrama
+    {
+        public string palavraOrigem { get; set; }
+        public List<string> listaAnagramas = new List<string>(); 
+
+        public void VerificarSeNomeEhVazioOuNulo()
+        {
+            if(String.IsNullOrEmpty(palavraOrigem))
+                throw new Exception("Palavra origem não pode ser nula ou vazia.");
+        }
+
+        // procedure GerarAnagramas(string: palavra)
+        public void GerarAnagramas(string palavra)
+        {
+            string novaPalavra = "";
+            string palavraOrigem = palavra;
+            var random = new Random();
+            var numeroCombinacoes = ObterNumeroDeCombinacoes(palavraOrigem);
+
+            while (listaAnagramas.Count < numeroCombinacoes)
+            {
+                palavra = palavraOrigem;
+                
+                while (palavra.Length > 0)
+                {                
+                    var indiceLetra = random.Next(0, palavra.Length);
+                    novaPalavra += palavra[indiceLetra].ToString();
+                    palavra = palavra.Remove(indiceLetra, 1);
+                }
+
+                if (!listaAnagramas.Contains(novaPalavra))
+                    listaAnagramas.Add(novaPalavra);
+
+                novaPalavra = "";
+            }
+
+            //if (listaAnagramas.Count < this.ObterNumeroDeCombinacoes(palavraOrigem))
+            //    GerarAnagramas(palavraOrigem);
+
+        }
+
+        // function
+        public int ObterNumeroDeCombinacoes(string palavra)
+        {
+            var quantidadeLetras = palavra.Length;
+            var multiplicador = quantidadeLetras;
+            
+            while (multiplicador > 1)
+            {
+                quantidadeLetras *= (--multiplicador);
+
+            }
+
+            return quantidadeLetras;
+
+        }
+
+    }
+}
